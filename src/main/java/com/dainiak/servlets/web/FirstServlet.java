bde128be5fad9d86858cb7c3842ffdedfab1a020
sasa <zhuchock@gmail.com>
2016-05-13 17:49:44 +0300

    redirect1

--- src/main/java/com/dainiak/servlets/web/FirstServlet.java
+++ src/main/java/com/dainiak/servlets/web/FirstServlet.java
@@ -18,7 +18,7 @@ public class FirstServlet extends HttpServlet {
    private static final Logger LOG = getLogger(FirstServlet.class);
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      LOG.debug("redirect to userList");
+      LOG.debug("redirect to userList1");
 
 //        request.getRequestDispatcher("/userList.jsp").forward(request, response);
         response.sendRedirect("index2.jsp");
