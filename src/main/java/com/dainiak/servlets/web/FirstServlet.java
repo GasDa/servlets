ae96696a7530a71a6a5c48e40cd25afb76a01229
sasa <zhuchock@gmail.com>
2016-05-13 11:32:00 +0300

    servlets

--- src/main/java/com/dainiak/servlets/web/FirstServlet.java
+++ src/main/java/com/dainiak/servlets/web/FirstServlet.java
@@ -15,10 +15,10 @@ import static org.slf4j.LoggerFactory.getLogger;
  * Created by adaynyak on 11.05.2016.
  */
 public class FirstServlet extends HttpServlet {
-  //  private static final Logger LOG = getLogger(UserServlet.class);
+   private static final Logger LOG = getLogger(FirstServlet.class);
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      //  LOG.debug("redirect to userList");
+      LOG.debug("redirect to userList");
 
 //        request.getRequestDispatcher("/userList.jsp").forward(request, response);
         response.sendRedirect("index2.jsp");
